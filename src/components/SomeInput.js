2ddefd452b8ecb88d7df6e1cd60aac8235b73c61
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2023-03-09 15:10:46 +0300

    Added validation in inputName for form..

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,9 +1,45 @@
-const SomeInput = (props) => {
+import { useState, useRef } from "react";
+
+const SomeInput = () => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [isEnteredNameValid, setIsEnteredNameValid] = useState(true);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setIsEnteredNameValid(false);
+      return;
+    }
+
+    setIsEnteredNameValid(true);
+
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+    setEnteredName("");
+  };
+
+  const nameInputClasses = isEnteredNameValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
+        {!isEnteredNameValid && <p className="error-text">Введите имя</p>}
       </div>
       <div className="form-actions">
         <button>Отправить</button>
